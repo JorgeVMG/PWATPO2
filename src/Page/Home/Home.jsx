ae908af29fae3cf3eaa83b838115ce0ce1179ce6
JorgeVMG <musicag230204@gmail.com>
2026-05-08 23:58:15 -0300

    inicios

---
+++ src/Page/Home/Home.jsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+    return (
+        <div className="bg-blue-500 border-b-2 m-3 h-100">
+            <div>
+                <h1>Hola</h1>
+            </div>
+        </div>
+    )
+}
